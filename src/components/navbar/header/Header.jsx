710ff2ee55c47d8dc79ea8f955b374824148fcad
MccGrey <chuxzzie01@yahoo.com>
2023-03-17 21:56:24 +0100

    add header section

--- src/components/navbar/header/Header.jsx
+++ src/components/navbar/header/Header.jsx
@@ -1,15 +1,22 @@
 import React from "react";
+import IMG1 from "../../../assets/trafalgar-header illustration 1.png";
+import "./header.css";
 
 const Header = () => {
   return (
     <>
-      <div>
-        <div>
-          <h2>fffffadqwfef</h2>
-          <p>ffffff</p>
-          <button> ffvfvf</button>
+      <div className="main">
+        <div className="header_text">
+          <h2>Virtual healthcare for you</h2>
+          <p>
+            Trafalgar provides progressive, and affordable healthcare,
+            accessible on mobile and online for everyone
+          </p>
+          <button> Consult today</button>
         </div>
-        <div></div>
+        <div className="image">
+          <img src={IMG1} alt="" />
+        </div>
       </div>
     </>
   );
