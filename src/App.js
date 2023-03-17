aa25ab45d72f228bcbc75978462f90f06f7ac838
MccGrey <chuxzzie01@yahoo.com>
2023-03-17 21:20:05 +0100

    add images

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import Header from "./components/navbar/header/Header";
 import Navbar from "./components/navbar/Navbar";
 
 function App() {
   return (
     <div>
       <Navbar />
+      <Header />
     </div>
   );
 }
---
+++ src/components/navbar/header/Header.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <>
+      <div>
+        <div>
+          <h2>fffffadqwfef</h2>
+          <p>ffffff</p>
+          <button> ffvfvf</button>
+        </div>
+        <div></div>
+      </div>
+    </>
+  );
+};
+
+export default Header;
