0a5c019d61b3c739b997a42e7718eb97f0d77b3e
MccGrey <chuxzzie01@yahoo.com>
2023-03-19 14:43:39 +0100

    testimonials

--- src/components/testimonial/Testimonial.jsx
+++ src/components/testimonial/Testimonial.jsx
@@ -2,12 +2,19 @@ import React from "react";
 import "./testimonial.css";
 import IMG3 from "../../assets/image 1.png";
 
+import { Swiper, SwiperSlide } from "swiper/react";
+
+import "swiper/swiper-bundle.min.css";
+import "swiper/swiper.min.css";
+
+import { Pagination } from "swiper";
+
 const Testimonial = () => {
   return (
     <>
       <section className="testimonial">
         <div>
-          <article className="testimonial-container">
+          <SwiperSlide className="testimonial-container">
             <div className="testimonial-header">
               <h3>What our customer are saying</h3>
               <div className="bar"></div>
@@ -31,9 +38,9 @@ const Testimonial = () => {
                 </p>
               </div>
             </div>
-          </article>
+          </SwiperSlide>
 
-          <article className="testimonial-container">
+          <SwiperSlide className="testimonial-container">
             <div className="testimonial-header">
               <h3>What our customer are saying</h3>
               <div className="bar"></div>
@@ -57,59 +64,7 @@ const Testimonial = () => {
                 </p>
               </div>
             </div>
-          </article>
-
-          <article className="testimonial-container">
-            <div className="testimonial-header">
-              <h3>What our customer are saying</h3>
-              <div className="bar"></div>
-            </div>
-
-            <div className="testimonial-card">
-              <div className="testimonial-info">
-                <div className="testimonial-image">
-                  <img src={IMG3} alt="img" />
-                </div>
-                <div className="testimonial-name">
-                  <h4>Edward Newgate</h4>
-                  <h6>Founder Circle</h6>
-                </div>
-              </div>
-              <div className="testimonial-content">
-                <p>
-                  “Our dedicated patient engagement app and web portal allow you
-                  to access information instantaneously (no tedeous form, long
-                  calls, or administrative hassle) and securely”
-                </p>
-              </div>
-            </div>
-          </article>
-
-          <article className="testimonial-container">
-            <div className="testimonial-header">
-              <h3>What our customer are saying</h3>
-              <div className="bar"></div>
-            </div>
-
-            <div className="testimonial-card">
-              <div className="testimonial-info">
-                <div className="testimonial-image">
-                  <img src={IMG3} alt="img" />
-                </div>
-                <div className="testimonial-name">
-                  <h4>Edward Newgate</h4>
-                  <h6>Founder Circle</h6>
-                </div>
-              </div>
-              <div className="testimonial-content">
-                <p>
-                  “Our dedicated patient engagement app and web portal allow you
-                  to access information instantaneously (no tedeous form, long
-                  calls, or administrative hassle) and securely”
-                </p>
-              </div>
-            </div>
-          </article>
+          </SwiperSlide>
         </div>
       </section>
     </>
